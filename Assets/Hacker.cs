9f033509dd27ac3c33a901d6e946def9875a9bac
Dimitry K <dkovalenko@ace-st.com>
2018-12-04 09:51:28 -0600

    add source code

---
+++ Assets/Hacker.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hacker : MonoBehaviour {
+
+    // Game states
+    int level;
+
+    enum Screen {MainMenu, Password, Win}
+    Screen currentScreen;
+
+    // Use this for initialization
+    void Start ()
+    {
+        currentScreen = Screen.MainMenu;
+        ShowMainMenu();
+    }
+
+    void ShowMainMenu()
+    {
+        currentScreen = Screen.MainMenu;
+        Terminal.ClearScreen();
+        Terminal.WriteLine("Choose your target:");
+        Terminal.WriteLine("Press 1 for mobile phone");
+        Terminal.WriteLine("Press 2 for laptop");
+        Terminal.WriteLine("Press 3 for military satelite");
+        Terminal.WriteLine("Enter your selection:");
+    }
+
+    void OnUserInput(string input)
+    {
+        if (input == "Menu" || input == "menu")
+        {
+            currentScreen = Screen.MainMenu;
+            ShowMainMenu();
+        }
+        else if (currentScreen == Screen.MainMenu)
+        {
+            RunMainMenu(input);
+        }
+    }
+
+    void RunMainMenu(string input)
+    {
+        if (input == "1")
+        {
+            level = 1;
+            StartGame();
+        }
+        else if (input == "2")
+        {
+            level = 2;
+            StartGame();
+        }
+        else if (input == "3")
+        {
+            level = 3;
+            StartGame();
+        }
+        else
+        {
+            Terminal.WriteLine("Please choose a valid level");
+        }
+    }
+
+    void StartGame()
+    {
+        currentScreen = Screen.Password;
+        Terminal.WriteLine("You're on level " + level);
+        Terminal.WriteLine("Password:");
+    }
+}
